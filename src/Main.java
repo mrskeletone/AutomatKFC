e002f1ac30557ad16f73350c5e0acdb17c9f24e1
mrskeletone <mr_skeletone_1000@mail.ru>
2024-01-25 19:28:37 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -14,9 +14,11 @@ public class Main {
         double g=9.8;
         double P=plotnosty*g*H;
         double v=Math.sqrt((2*(P-Math.pow(10,5)))/plotnosty);
-        int N=(int)Math.ceil((Math.PI*H)/(nozzleDiameter*0.3*H));
+        double l=H*0.5;
+        int N=(int)Math.ceil((Math.PI*H)/(nozzleDiameter*l));
         double h=H-0.3*D;
         double angle=Math.atan(h/(D/2));
         double Q=(N*Math.PI*Math.pow(nozzleDiameter,2)*v)/4;
-        System.out.println(String.format("d=%.4f\nP=%.4f\nv=%.4f\nN=%d\nh=%.4f\nangle=%.4f\nQ=%.4f",nozzleDiameter,P,v,N,H-h,angle,Q));
+        System.out.println(String.format("Диаметр d=%.4f м.\nДавление P=%.4f Па\nКоличество сопел N=%d \nВысота сопел h=%.4f м\nУгол сопел angle=%.4f град.\n" +
+                "Расход жидкости Q=%.4f",nozzleDiameter,P,N,H-h,angle,Q));
     }}
